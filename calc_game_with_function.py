709e09f2c9b660bc2c5c643d0dd4c8f1831e3366
WONSEOK SHIN <neo1seok@gmail.com>
2019-05-16 11:15:44 +0900

    Create calc_game_with_function.py
    산수게임 함수 사용

---
+++ calc_game_with_function.py
@@ -0,0 +1,22 @@
+import random
+
+
+def math_game(try_time):
+#try_time  = 3
+ 	while  True:
+		a = random.randint(1, 100)
+		b = random.randint(1, 100)
+		print(a, "+", b, "=")
+		c = input()
+		d = int(c)
+		for _ in range(try_time):
+			if d == a+b:
+				print('당신은 산수쟁이')
+				break
+			else:
+				print(a+b, '당신은 돌대가리 %d 번 틀렸음'%(_+1))
+		
+		print('다음 문제')
+		
+if __name__ == '__main__':
+	math_game(3)		
